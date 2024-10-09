ae795db0982df0e911b1f2a2a6a9151b3657abaf
Roman Gurung <73432981+romang2103@users.noreply.github.com>
2024-10-09 12:33:02 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b
+
+print("Testing Python file...")
+print("2 + 3 =", add(2, 3))
+
+print("If you see this, your Python setup is working!")
